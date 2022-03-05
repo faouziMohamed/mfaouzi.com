053f8c74b349c230b13f4cc00cb8d3fd336676cd
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-03-05 04:19:34 +0100

    fix: svg fix component props

--- src/components/images/SpaceMan.tsx
+++ src/components/images/SpaceMan.tsx
@@ -4,11 +4,7 @@ export default function SpaceMan() {
   return (
     <div className='relative flex w-full flex-col items-center justify-center pt-14'>
       <div className='relative flex h-60 w-60 items-center justify-center'>
-        <SpaceManSVG
-          className='h-full w-full object-contain'
-          alt='Space man illustration'
-          title='Space man in space'
-        />
+        <SpaceManSVG className='h-full w-full object-contain' />
       </div>
     </div>
   );
