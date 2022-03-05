3e9c719b75bad4dfb675286fda11c42fcc42c96c
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-03-05 17:19:50 +0100

    fix(workaround tests): add mock for next/image

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,21 +1,32 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { render } from '@testing-library/react';
 
+import devData from '@/data/dev-data.json';
+
+import IntroSection from '@/components/home/IntroSection';
+import Layout from '@/components/layout/Layout';
+
 import HomePage from '@/pages';
 
-/** Mock Seo's useRouter */
-jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      asPath: '/',
-    };
-  },
-}));
+describe('Index Page', () => {
+  it('renders The Layout', async () => {
+    const { container } = render(
+      <Layout>
+        <p>Hello world</p>
+      </Layout>,
+    );
+    expect(container.firstChild?.hasChildNodes()).toBeTruthy();
+  });
+
+  test('render the intro section showing a png with next/image', async () => {
+    const { container } = render(<IntroSection data={devData} />);
+    expect(container.firstChild?.hasChildNodes()).toBeTruthy();
+  });
+});
 
 describe('Index Page', () => {
   it('renders index page', async () => {
     const { container } = render(<HomePage />);
-
     expect(container.firstChild?.hasChildNodes()).toBeTruthy();
   });
 });
