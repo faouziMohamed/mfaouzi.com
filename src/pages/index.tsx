b43c168f6fd8e3f8ff5362563c88f2075f67636d
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-07-18 02:01:41 +0100

    update:(responsivness) modify about and intro section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container as Main } from '@mui/material';
+import { Box, Container as Main, Stack } from '@mui/material';
 
 import devData from '@/data/dev-data.json';
 import projects from '@/data/projects.json';
@@ -8,7 +8,7 @@ import ContactSection from '@/components/home/ContactSection';
 import DownArrowIcon from '@/components/home/DownArrowIcon';
 import IntroSection from '@/components/home/IntroSection';
 import ProjectsSection from '@/components/home/ProjectsSection';
-import SkilsAndStackSection from '@/components/home/SkilsAndStackSection';
+import SkillsAndStackSection from '@/components/home/SkilsAndStackSection';
 import SocialsSection from '@/components/home/SocialsSection';
 import SpaceMan from '@/components/images/SpaceMan';
 import Layout from '@/components/layout/Layout';
@@ -22,17 +22,24 @@ export default function HomePage() {
   return (
     <Layout className='flex flex-col gap-4 p-0'>
       <Seo templateTitle='Home' title='Faouzi Mohamed' />
-      <SpaceMan />
       <Main component='main' className='flex flex-col gap-4 '>
-        <IntroSection data={data} className=' px-6' />
-        <SocialsSection data={data} className=' px-6' />
+        <Box className='relative flex w-full basis-6 flex-col md:grow md:pt-24'>
+          <SpaceMans />
+          <Box className='z-10 flex flex-col justify-center gap-2 md:flex-row md:items-center'>
+            <IntroSection
+              data={data}
+              className='grow basis-full px-6 md:px-4'
+            />
+            <AboutMeSection className='rounded border border-primary-200 bg-primary-100 bg-opacity-80 px-6 py-2' />
+          </Box>
+        </Box>
+        <SocialsSection data={data} className='basis-full px-6 md:px-4' />
         <DownArrowIcon className='text-5xl' />
-        <AboutMeSection className='px-6' />
         <ProjectsSection
           projects={projects as IProjectDataType[]}
           className='px-6'
         />
-        <SkilsAndStackSection className='px-6' />
+        <SkillsAndStackSection className='px-6' />
         <ContactSection className='px-6' />
       </Main>
     </Layout>
@@ -48,4 +55,16 @@ export interface ProjectData {
   forksCount: number;
   description: string;
   languages: { [key: string]: number };
+}
+function SpaceMans() {
+  return (
+    <>
+      <Box className=' top-0 animate-wiggle md:absolute md:opacity-50'>
+        <SpaceMan />
+      </Box>
+      <Box className='right-0 bottom-0 hidden animate-wiggle md:absolute md:block'>
+        <SpaceMan />
+      </Box>
+    </>
+  );
 }
