699b29488c3c55a87bfa9902b030e559e52869f0
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-09-13 14:14:28 +0000

    vercel: update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,11 +3,9 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
+    policies: [{ userAgent: '*', allow: '/' }]
+  }
 };
