9c892efe2b07cd966af613629e7180d3fdb28c72
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-10-27 16:21:25 +0100

    build: set package to next 13

--- src/components/NextImage.tsx
+++ src/components/NextImage.tsx
@@ -15,7 +15,7 @@ export default function NextImage({
   src,
   width,
   height,
-  layout,
+  // layout,
   alt,
   ...rest
 }: NextImageProps) {
@@ -26,7 +26,7 @@ export default function NextImage({
         src={src}
         width={width}
         height={height}
-        layout={layout}
+        // layout={layout}
         alt={alt}
         loader={customLoader}
         placeholder='blur'
@@ -45,18 +45,18 @@ function customLoader({ src, width, quality }: ImageLoaderProps): string {
 
 function shimmer(w: number, h: number) {
   return `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg width='${w}' height='${h}' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
   <defs>
-    <linearGradient id="g">
+    <linearGradient id='g'>
       <stop stop-color='#f6f7f8' offset='0%' />
       <stop stop-color='#edeef1' offset='20%' />
       <stop stop-color='#f6f7f8' offset='40%' />
       <stop stop-color='#f6f7f8' offset='70%' />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#f6f7f8" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <rect width='${w}' height='${h}' fill='#f6f7f8' />
+  <rect id='r' width='${w}' height='${h}' fill='url(#g)' />
+  <animate xlink:href='#r' attributeName='x' from='-${w}' to='${w}' dur='1s' repeatCount='indefinite'  />
 </svg>`;
 }
 
