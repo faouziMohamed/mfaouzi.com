a544dfd002b73df022e9aff427675969c578c68f
Faouzi Mohamed <hello@mfaouzi.com>
2023-01-22 16:14:15 +0000

    feat: add generated seo on server side before rendering

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -13,22 +13,7 @@ import getLdJsonStringified from '@/components/ldJsonDataDefinition';
 import theme from '@/themes/theme';
 import { useNextTheme } from '@/themes/themeContext';
 
-const defaultMeta = {
-  title: 'Faouzi Mohamed',
-  siteName: "Faouzi Mohamed's Portfolio",
-  description:
-    'My personal Portfolio where I present myself, my skills, some projects etc. ',
-  url: process.env.NEXT_PUBLIC_SITE_URL,
-  pathname: '/',
-  type: 'website',
-  robots: 'follow, index',
-  locale: 'en_US',
-  imageWidth: '1200',
-  imageHeight: '630',
-  ogId: process.env.NEXT_PUBLIC_FB_APP_ID,
-  keywords:
-    'faouzi, mohamed,Faouzi Mohamed, Portfolio, developer, web developer, full-stack developer, backend developer, resume',
-};
+import defaultMeta from './seoDefault';
 
 type SeoProps = {
   image?: string;
@@ -53,7 +38,7 @@ export default function Seo(props: SeoProps) {
       description: meta.description,
       siteName: props.templateTitle ? meta.siteName : meta.title,
       templateTitle: props.templateTitle,
-      logo: 'https://avatars.githubusercontent.com/u/57812398?&v=4',
+      logo: `${process.env.NEXT_PUBLIC_SITE_URL}/images/faouzi-mhd.jpeg`,
       theme: themeMode,
     });
   const themeColor =
@@ -66,13 +51,7 @@ export default function Seo(props: SeoProps) {
       <meta name='robots' content={meta.robots} />
       <meta name='description' content={meta.description} />
       <link rel='canonical' href={`${meta.url}${pathname}`} />
-      <meta
-        name='site_map'
-        content={`${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`}
-      />
       <meta name='url' content={meta.url} />
-      <meta name='copyright' content='Faouzi Mohamed' />
-      <meta name='classification' content='portfolio' />
       <meta name='keywords' content={`${meta.keywords}`} />
 
       {/* Open Graph */}
@@ -82,49 +61,17 @@ export default function Seo(props: SeoProps) {
       <meta property='og:description' content={meta.description} />
       <meta property='og:Name' content={meta.title} />
       <meta property='og:image' content={meta.image} name='image' />
-      <meta property='og:image:width' content={meta.imageWidth} />
-      <meta property='og:image:height' content={meta.imageHeight} />
-      <meta name='fb:app_id' property='fb:app_id' content={meta.ogId} />
 
       {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content='@fz_faouzi' />
       <meta name='twitter:Name' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
       <meta name='twitter:image' content={meta.image} />
-      <meta name='twitter:image:alt' content={"Faouzi Mohamed's Portfolio"} />
-      <meta name='twitter:creator' content='@fz_faouzi' />
-
-      {meta.date && (
-        <>
-          <meta
-            name='publish_date'
-            property='og:publish_date'
-            content={meta.date}
-          />
-          <meta
-            name='author'
-            property='article:author'
-            content='Faouzi Mohamed'
-          />
-        </>
-      )}
-
-      <meta
-        name='apple-mobile-web-app-Name'
-        content={process.env.NEXT_PUBLIC_APP_NAME}
-      />
-      <meta
-        name='application-name'
-        content={process.env.NEXT_PUBLIC_APP_NAME}
-      />
-      <meta name='apple-mobile-web-app-capable' content='yes' />
-      <meta name='apple-touch-fullscreen' content='yes' />
 
       {/* Theme */}
       <meta name='theme-color' content={themeColor} />
       <meta name='apple-mobile-web-app-status-bar-style' content={themeColor} />
       <meta name='msapplication-TileColor' content={themeColor} />
+
       {/*  JSON LD  */}
       <script
         type='application/ld+json'
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, react/no-danger */
 
 import createEmotionServer from '@emotion/server/create-instance';
+import getConfig from 'next/config';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { Children } from 'react';
+
+import { openGraphImage } from '@/lib/helper';
+
+import getLdJsonStringified from '@/components/ldJsonDataDefinition';
+import seoDefault from '@/components/seoDefault';
 
 import theme from '@/themes/theme';
 import createEmotionCache from '@/utils/createEmotionCache';
 
+const ogImg = openGraphImage({
+  description: seoDefault.description,
+  siteName: seoDefault.siteName,
+  templateTitle: seoDefault.siteName,
+  logo: `${process.env.NEXT_PUBLIC_SITE_URL}/images/faouzi-mhd.jpeg`,
+  theme: 'light',
+});
+
+const { publicRuntimeConfig } = getConfig();
+const { lastBuild } = publicRuntimeConfig;
 class MyDocument extends Document {
   render() {
     return (
@@ -16,6 +34,70 @@ class MyDocument extends Document {
           <meta name='theme-color' content={theme.palette.primary.main} />
           <meta charSet='UTF-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='robots' content={seoDefault.robots} />
+          <meta name='description' content={seoDefault.description} />
+          <meta
+            name='site_map'
+            content={`${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`}
+          />
+          <meta property='og:Name' content={seoDefault.title} />
+          <meta property='og:site_name' content={seoDefault.siteName} />
+          <meta property='og:description' content={seoDefault.description} />
+          <meta property='og:image' content={ogImg} name='image' />
+          <meta property='og:image:width' content={seoDefault.imageWidth} />
+          <meta property='og:image:height' content={seoDefault.imageHeight} />
+          <meta
+            name='fb:app_id'
+            property='fb:app_id'
+            content={seoDefault.ogId}
+          />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:site' content='@fz_faouzi' />
+          <meta
+            name='twitter:image:alt'
+            content={"Faouzi Mohamed's Portfolio"}
+          />
+          <meta name='twitter:creator' content='@fz_faouzi' />
+
+          {lastBuild ? (
+            <>
+              <meta
+                name='publish_date'
+                property='og:publish_date'
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                content={lastBuild}
+              />
+              <meta
+                name='author'
+                property='article:author'
+                content='Faouzi Mohamed'
+              />
+            </>
+          ) : null}
+
+          <meta name='copyright' content='Faouzi Mohamed' />
+          <meta name='classification' content='portfolio' />
+
+          <meta
+            name='apple-mobile-web-app-Name'
+            content={process.env.NEXT_PUBLIC_APP_NAME}
+          />
+          <meta
+            name='application-name'
+            content={process.env.NEXT_PUBLIC_APP_NAME}
+          />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-touch-fullscreen' content='yes' />
+          <script
+            type='application/ld+json'
+            dangerouslySetInnerHTML={{
+              __html: getLdJsonStringified({
+                siteUrl: seoDefault.url!,
+                dateModified: lastBuild,
+                ogImage: ogImg,
+              }),
+            }}
+          />
           {/* @ts-expect-error: attribute not existing */}
           {this.props.emotionStyleTags}
         </Head>
