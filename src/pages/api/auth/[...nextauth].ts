3bb337ba64e55ef0989093432e27608187fdf37f
Faouzi Mohamed <me@mfaouzi.com>
2023-03-11 22:21:01 +0100

    fix: revert back to custom authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,40 +1,51 @@
-/* eslint-disable no-console */
-import NextAuth from 'next-auth';
+import NextAuth, { Account, Profile } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import GithubProvider from 'next-auth/providers/github';
 
-// type ProviderProfile = Profile & { avatar_url: string; login: string };
+import { GUESTBOOK_PAGE } from '@/lib/client-route.contant';
+import { capitalize } from '@/lib/utils';
 
-// function assembleNewUser(account: Account, profile: ProviderProfile) {
-//   const user: AppUserWithEmail = {
-//     providerId: account.providerAccountId,
-//     email: profile.email!,
-//     avatar: profile.avatar_url,
-//     name: capitalize(profile.name || profile.login),
-//     providerName: account.provider,
-//   };
-//   return user;
-// }
+import {
+  addNewUser,
+  getUserByProviderId,
+  updateUser,
+} from '@/Repository/guestbook.queries';
 
-// function getUpdatedFields(maybeUser: AppUserWithEmail, profile: Profile) {
-//   const updatedFields: Partial<AppUserWithEmail> = {};
-//   if (maybeUser.email !== profile.email) {
-//     updatedFields.email = profile.email;
-//   }
-//   if (maybeUser.avatar !== profile.image) {
-//     updatedFields.avatar = profile.image!;
-//   }
-//   if (maybeUser.name !== profile.name) {
-//     updatedFields.name = profile.name!;
-//   }
-//   return updatedFields;
-// }
+import { AppUser, AppUserWithEmail } from '@/types/guestbook/guestbook.types';
 
-// type ObjectWithUser<T extends object> = T & { user: AppUser };
+type ProviderProfile = Profile & { avatar_url: string; login: string };
+
+function assembleNewUser(account: Account, profile: ProviderProfile) {
+  const user: AppUserWithEmail = {
+    providerId: account.providerAccountId,
+    email: profile.email!,
+    avatar: profile.avatar_url,
+    name: capitalize(profile.name || profile.login),
+    providerName: account.provider,
+  };
+  return user;
+}
+
+function getUpdatedFields(maybeUser: AppUserWithEmail, profile: Profile) {
+  const updatedFields: Partial<AppUserWithEmail> = {};
+  if (maybeUser.email !== profile.email) {
+    updatedFields.email = profile.email;
+  }
+  if (maybeUser.avatar !== profile.image) {
+    updatedFields.avatar = profile.image!;
+  }
+  if (maybeUser.name !== profile.name) {
+    updatedFields.name = profile.name!;
+  }
+  return updatedFields;
+}
+
+type ObjectWithUser<T extends object> = T & { user: AppUser };
 export default NextAuth({
-  // pages: {
-  //   error: GUESTBOOK_PAGE,
-  //   signIn: GUESTBOOK_PAGE,
-  // },
+  pages: {
+    error: GUESTBOOK_PAGE,
+    signIn: GUESTBOOK_PAGE,
+  },
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -42,54 +53,46 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-  // callbacks: {
-  //   jwt({ token, user, account }) {
-  //     // user and account are only available on sign in
-  //     console.log('jwt STARTED', { token, user, account }, '\n\n');
-  //     if (user) {
-  //       const tk = token as ObjectWithUser<JWT>;
-  //       tk.user = {
-  //         id: user.id,
-  //         avatar: '/images/faouzi-mhd.jpeg',
-  //         name: capitalize(user.name!),
-  //       };
-  //     }
-  //     console.log('jwt', { token }, '\n\n');
-  //     return token;
-  //   },
-
-  //   session({ session, token }) {
-  //     console.log('session STARTED', { session });
-  //     const tk = token as ObjectWithUser<JWT>;
-  //     session.user = tk.user;
-  //     console.log('session ENDED', { session }, '\n\n');
-  //     return session;
-  //   },
-  //   async signIn(props) {
-  //     const { account, profile } = props;
-  //     console.log('signIn STARTED', { account, profile });
-  //     try {
-  //       // If the user doesn't exist, create a new user
-  //       const maybeUser = await getUserByProviderId(account!.providerAccountId);
-  //       console.log('maybeUser', { maybeUser });
-  //       if (!maybeUser) {
-  //         const providerProfile = profile as ProviderProfile;
-  //         const newUser = assembleNewUser(account!, providerProfile);
-  //         await addNewUser(newUser);
-  //         return true;
-  //       }
-  //       // check if the avatar, email and the name are the same as the one in the database
-  //       const updatedFields = getUpdatedFields(maybeUser, profile!);
-  //       if (Object.keys(updatedFields).length > 0) {
-  //         await updateUser(maybeUser.providerId, updatedFields);
-  //       }
-  //       console.log('SIGN IN ENDED', maybeUser, '\n\n');
-  //       return true;
-  //     } catch (error) {
-  //       // eslint-disable-next-line no-console
-  //       console.log(error);
-  //       return false;
-  //     }
-  //   },
-  // },
+  callbacks: {
+    jwt({ token, user }) {
+      // user and account are only available on sign in
+      if (user) {
+        const tk = token as ObjectWithUser<JWT>;
+        tk.user = {
+          id: user.id,
+          avatar: user.image!,
+          name: capitalize(user.name!),
+        };
+      }
+      return token;
+    },
+    session({ session, token }) {
+      const tk = token as ObjectWithUser<JWT>;
+      session.user = tk.user;
+      return session;
+    },
+    async signIn(props) {
+      const { account, profile } = props;
+      try {
+        // If the user doesn't exist, create a new user
+        const maybeUser = await getUserByProviderId(account!.providerAccountId);
+        if (!maybeUser) {
+          const providerProfile = profile as ProviderProfile;
+          const newUser = assembleNewUser(account!, providerProfile);
+          await addNewUser(newUser);
+          return true;
+        }
+        // check if the avatar, email and the name are the same as the one in the database
+        const updatedFields = getUpdatedFields(maybeUser, profile!);
+        if (Object.keys(updatedFields).length > 0) {
+          await updateUser(maybeUser.providerId, updatedFields);
+        }
+        return true;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
